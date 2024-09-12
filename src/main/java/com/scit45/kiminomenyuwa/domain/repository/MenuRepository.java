adbd2d42c64e505ef788c4a3f59f376ae176a945
Yoon12345678 <heeyoonkim07@gmail.com>
2024-09-12 15:40:02 +0900

    하기싫어

--- src/main/java/com/scit45/kiminomenyuwa/domain/repository/MenuRepository.java
+++ src/main/java/com/scit45/kiminomenyuwa/domain/repository/MenuRepository.java
@@ -3,12 +3,23 @@ package com.scit45.kiminomenyuwa.domain.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.scit45.kiminomenyuwa.domain.entity.MenuEntity;
 
 @Repository
 public interface MenuRepository extends JpaRepository<MenuEntity, Integer> {
-	// 가게 ID로 메뉴 목록 조회
+	/**
+	    * 해당 유저가 아직 평가하지 않은 메뉴 리스트를 가져오는 쿼리.
+	    *
+	    * @param userId 평가를 조회할 사용자 ID
+	    * @return 해당 사용자가 평가하지 않은 메뉴들의 리스트
+	    */
+	@Query("SELECT m FROM MenuEntity m WHERE m.menuId NOT IN (SELECT r.menuId FROM MiniGameMenuRatingEntity r WHERE r.userId = :userId)")
+	List<MenuEntity> findUnratedMenusByUserId(@Param("userId")
+	String userId);
+
 	List<MenuEntity> findByStoreId(Integer storeId);
 }
