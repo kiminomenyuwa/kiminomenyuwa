63e39917cd737ddf27dd35488a06dc0fe2cec427
ossor322 <ossor322@gmail.com>
2024-09-20 00:07:28 +0900

    chore: aaa user insert문 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,9 @@
 -- 현재 모든 테스트 데이터가 userID : 'aaa'를 기준으로 되어있고. 회원가입의 경우 비밀번호 암호화때문에 직접 해줘야하는 듯
 use kiminomenyuwa;
 
+INSERT INTO `user` (`user_id`, `password_hash`, `name`, `birth_date`, `gender`, `email`, `road_name_address`, `detail_address`, `zipcode`, `phone_number`, `profile_img_uuid`, `role`, `enabled`)
+VALUES ('aaa', '$2a$10$IqLsIOXCXOcJjMg2Gz7gW.vEBJP02BfIT6kWfcBVdWkraI5/Y.1JS', '김철수', '1985-10-15', 'male', 'testuser01@example.com', '서울시 강남구', '테헤란로 123', '06234', '010-1234-5678', 'http://example.com/photo.jpg', 'ROLE_USER', 1);
+
 -- 카테고리 타입 테이블에 데이터 추가
 INSERT INTO category_type (type_name)
 VALUES ('재료'),
