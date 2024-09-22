394065cd27148fd8ee19fbcf3a851acf4e64071b
ossor322 <ossor322@gmail.com>
2024-09-22 23:35:48 +0900

    Feat: StorePhotoEntity 추가 및 schema.sql 수정

---
+++ src/main/java/com/scit45/kiminomenyuwa/domain/entity/StorePhotoEntity.java
@@ -0,0 +1,35 @@
+package com.scit45.kiminomenyuwa.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 가게에 속한 사진 정보를 저장하는 엔티티 클래스입니다.
+ */
+@Data
+@Entity
+@Table(name = "store_photo")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StorePhotoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "photo_id")
+    private Integer photoId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private StoreEntity store;
+
+    @Column(name = "photo_url", nullable = false)
+    private String photoUrl;
+
+    // 대표 사진 여부를 나타내는 필드
+    @Column(name = "is_main")
+    private Boolean isMain = false;
+}
