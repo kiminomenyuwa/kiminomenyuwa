fc130329eb6c465a235ccee72102a0dd948176e6
ossor322 <ossor322@gmail.com>
2024-09-28 19:31:48 +0900

    feat: store 및 menu 관련 insert문 sql 파일 추가

---
+++ src/main/resources/category_type.sql
@@ -0,0 +1,14 @@
+INSERT INTO `category_type`
+VALUES ('1', '대분류');
+INSERT INTO `category_type`
+VALUES ('2', '소분류');
+INSERT INTO `category_type`
+VALUES ('3', '재료');
+INSERT INTO `category_type`
+VALUES ('4', '조리방법');
+INSERT INTO `category_type`
+VALUES ('5', '식이제한');
+INSERT INTO `category_type`
+VALUES ('6', '음식특성');
+INSERT INTO `category_type`
+VALUES ('7', '식사시간');
